fabbdea0e3ca14f8832fec2294c40878f140f619
danielsason112 <danielsason112@gmail.com>
2020-11-05 20:51:20 +0200

    Initial commit

---
+++ src/main/java/demo/NameBoundary.java
@@ -0,0 +1,34 @@
+package demo;
+
+public class NameBoundary {
+	
+	private String first;
+	private String last;
+	
+	public NameBoundary() {
+	}
+
+	public NameBoundary(String first, String last) {
+		this.first = first;
+		this.last = last;
+	}
+
+	public String getFirst() {
+		return first;
+	}
+
+	public void setFirst(String first) {
+		this.first = first;
+	}
+
+	public String getLast() {
+		return last;
+	}
+
+	public void setLast(String last) {
+		this.last = last;
+	}
+	
+	
+
+}
