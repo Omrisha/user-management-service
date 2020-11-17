be75a371b33a4e76f946eabf8c3b1977143423fb
Omri Shapira <omri.shapira@gmail.com>
2020-11-17 23:19:31 +0200

    fix

--- src/main/java/demo/layout/UserManagmentController.java
+++ src/main/java/demo/layout/UserManagmentController.java
@@ -1,5 +1,6 @@
 package demo;
 
+import demo.layout.UserBoundary;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.validation.BindingResult;
